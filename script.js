345e94cd584b60b193b28f45fd298c246363b992
Megapixar86 <lysenyi_n@bk.ru>
2024-10-23 18:35:25 +0300

    добавил правильные функции для расчета дальности и УЗД. Добавил параметр частота

--- script.js
+++ script.js
@@ -13,7 +13,15 @@
 	let ob
 	let ang
 	let spl
+	let uzd
+	let spls
 	let selAng
+	let power_calc
+	let lenght
+	let height
+	let len_u
+	let arr_dist
+	let arr_uzd
 	//let rupor
 	//функция загрузки данных
 	async function getData(address, sht){
@@ -100,12 +108,19 @@
 		}
 		return num
 	}
+	//функция расчета дальности
+	let dist = (spl, power, uzd) => 10**((spl+10*Math.log10(power)-uzd)/20)
+	//функция расчета звукового давления
+	let UZDofdist = (spl, power, dist) => spl + 10*Math.log10(power) - 20*Math.log10(dist)
 
 	function calc(){
 		//drawClear()
+		arr_dist = []
+		arr_uzd = []
 		try {
 			//Высота установки
-			let height = +el("inh").value
+			height = +el("inh").value
+			//console.log(height)
 			//console.log(height)
 			if(isNaN(height) || height === 0){
 				//height = 0;
@@ -116,31 +131,72 @@
 			let area = + el("sq").value
 			//console.log(area)
 			if(isNaN(area) || area === 0 ){
-				throw new Error("Данные не верны");
+				throw new Error("Данные площади не верны");
 			}
 			//Уровень шума
 			let noise = +el("noise_inp").value
 			//console.log(noise)
 			if(isNaN(noise) || noise === 0 ){
-				throw new Error("Данные не верны");
+				throw new Error("Данные шума не верны");
 			}
-			let uzd = +el("uzd_inp").value
+			uzd = +el("uzd_inp").value
+			//console.log(uzd)
+			ob = el("sel").selectedIndex
+
 			if(el("exec").options[el("exec").selectedIndex].text === "Потолочный"){
 				//console.log("Монтируем в потолок")
-				for(obj in objs_w){
-					if(objs_p[obj]['Модель'] === el("sel").options[el("sel").selectedIndex].text){
-						ob = obj
-						break
+				
+				power = objs_p[ob]['Мощность, Вт'].split("/")
+				spl = +objs_p[ob]['SPL, дБ']
+				
+				for(let elem of power){
+					let d = dist(spl, elem, uzd)
+					let uzdL = UZDofdist(spl, elem, height-1.5)
+					if(uzdL<120){
+						arr_dist.push(d)
+						arr_uzd.push(uzdL)
 					}
+					//elem
 				}
-				let spls= objs_p[ob]['Мощность, Вт'].split("/")
-				//let power_calc = 10**(uzd - spl + 20 * Math.log10(1.4*(height-1.5)))
-				console.log(spl)
+				
+				//lenght = dist(spl, power[], uzd)
+				//len_u = (height-1.5)/Math.cos(90*Math.PI/180)
+				//len_u = height
+				//power_calc = 10**((spl + 20 * Math.log10(len_u) - uzd )/10)
+				//power_calc = 10**(uzd - spl + 20 * Math.log10( len_u) )/10
+				//uzd=+spl + 10 * Math.log10(power)
+				//10**(75-95)/10
+				//lenght = 10**((spl + 10 * Math.log10(+power[4]) - uzd)/20)
+				//console.log(objs_p[ob]['Мощность, Вт'])
 				//if(objs_w[obj]['Модель'])
 			}
+			if(el("exec").options[el("exec").selectedIndex].text === "Настенный"){
+				power = objs_w[ob]['Мощность, Вт'].split("/")
+				spl = +objs_w[ob]['SPL, дБ']
+				for(let elem of power){
+					let d = dist(spl, elem, uzd)
+					let uzdL = UZDofdist(spl, elem, height-1.5)
+					if(uzdL<120){
+						arr_dist.push(d)
+						arr_uzd.push(uzdL)
+					}
+					//elem
+				}
+			}
+			let areaL = (arr_dist[arr_dist.length-1]**2 - (height - 1.5)**2)*Math.PI
+			el("row0").value = objs_p[ob]['Модель']
+			el("row1").value = power[power.length-1]
+			el("row2").value = areaL
+			el("row3").value = area/areaL
+			/*console.log(spl)
+			console.log(len_u)
+			console.log(power_calc)
+			console.log(lenght)*/
 			//узд
 			//let uzd = noise + 15
 			//console.log(uzd)
+			console.log(arr_dist)
+			console.log(arr_uzd)
 			
 		}catch (err) {
 			console.log(err)
